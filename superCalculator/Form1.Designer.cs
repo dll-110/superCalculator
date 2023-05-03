3a6213f99b9b85869e49c0de048aa9172524f085
Gogs <gogs@fake.local>
2023-05-03 19:41:20 +0800

    1

---
+++ superCalculator/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace superCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(718, 866);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
+            this.richTextBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.richTextBox1_PreviewKeyDown);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(718, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "始终最前";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.statusStrip1);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(718, 866);
+            this.panel1.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 837);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(718, 29);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(44, 24);
+            this.toolStripStatusLabel1.Text = "^_^";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 891);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "好方便的计算器";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ superCalculator/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace superCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        decimal lastResult = 0;
+
+
+        // 设置此窗体为活动窗体：
+        // 将创建指定窗口的线程带到前台并激活该窗口。键盘输入直接指向窗口，并为用户更改各种视觉提示。
+        // 系统为创建前台窗口的线程分配的优先级略高于其他线程。
+        [DllImport("user32.dll", EntryPoint = "SetForegroundWindow")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        // 设置此窗体为活动窗体：
+        // 激活窗口。窗口必须附加到调用线程的消息队列。
+        [DllImport("user32.dll", EntryPoint = "SetActiveWindow")]
+        public static extern IntPtr SetActiveWindow(IntPtr hWnd);
+
+        // 设置窗体位置
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int Width, int Height, int flags);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 设置窗体显示在最上层
+            SetWindowPos(this.Handle, -1, 0, 0, 0, 0, 0x0001 | 0x0002 | 0x0010 | 0x0080);
+
+            // 设置本窗体为活动窗体
+            SetActiveWindow(this.Handle);
+            SetForegroundWindow(this.Handle);
+
+            // 设置窗体置顶
+            this.TopMost = true;
+        }
+
+        private decimal Calculate(string input)
+        {
+            // 利用DataTable的Compute方法计算表达式的结果
+            if (!input.Trim().Equals(""))
+            {
+                DataTable dt = new DataTable();
+                object result = dt.Compute(input, "");
+                return Convert.ToDecimal(result);
+            }
+            return 0;
+        }
+
+        private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            try
+            {
+                int cursorLine = richTextBox1.GetLineFromCharIndex(richTextBox1.SelectionStart);
+                int start = richTextBox1.GetFirstCharIndexFromLine(cursorLine);
+                int end;
+                if (cursorLine + 1 < richTextBox1.Lines.Count())
+                {
+                    end = richTextBox1.GetFirstCharIndexFromLine(cursorLine + 1);
+                }
+                else
+                {
+                    end = richTextBox1.Text.Length;
+                }
+                string lineT = richTextBox1.Text.Substring(start, end - start);
+
+
+                if (e.KeyChar == '=' || e.KeyChar == 13 || (e.KeyChar == '+' && lineT.EndsWith("+")))
+                {
+                    // 提取等号前面的内容
+                    string input = richTextBox1.Lines.LastOrDefault();
+                    int index = input.LastIndexOf('=');
+                    if (index != -1)
+                    {
+                        input = input.Substring(0, index);
+                    }
+
+                    if (input.EndsWith("+"))
+                    {
+                        input = input.Substring(0, input.Length - 1);
+                    }
+
+
+                    // 计算表达式的结果
+                    lastResult = Calculate(input);
+                    if (input.EndsWith("+"))
+                    {
+                        richTextBox1.AppendText("=" + lastResult);
+                    }
+                    else
+                    {
+                        richTextBox1.AppendText("=" + lastResult);
+                    }
+
+                    // 在下一行添加结果
+                    richTextBox1.AppendText(Environment.NewLine);
+                    e.Handled = true;
+                }
+
+
+                int cursorPos = richTextBox1.SelectionStart;
+
+
+
+                int lineStartPos = richTextBox1.GetFirstCharIndexFromLine(richTextBox1.GetLineFromCharIndex(cursorPos));
+
+                // 如果当前光标位置在行首，则表示当前行是新的一行
+                bool isNewLine = cursorPos == lineStartPos;
+
+                if (e.KeyChar == '+' && lineT.EndsWith("+"))
+                {
+                    return;
+                }
+
+                if ((e.KeyChar == '+' || e.KeyChar == '-' || e.KeyChar == '*' || e.KeyChar == '/') && isNewLine)
+                {
+                    // 复制上一行结果到当前行符号的前面
+                    string lastLine = richTextBox1.Lines.LastOrDefault();
+                    richTextBox1.AppendText(lastResult + "");
+                    //if (lastLine != null && lastLine.EndsWith("="))
+                    //{
+                    //    string result = lastLine.Substring(lastLine.LastIndexOf('=') + 1).Trim();
+                    //}
+                }
+            }
+            catch(Exception err)
+            {
+                this.toolStripStatusLabel1.Text = err.Message;
+            }
+        }
+
+        private void richTextBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+
+            string lastChars = "";
+
+
+            Ranks();
+
+
+
+           
+            // 如果当前字符是加号并且前一个字符也是加号，则将其替换为等号
+           // if (e.KeyCode == Keys.Add )
+          //  {
+                // 将两个加号替换为等号
+          //      SendKeys.Send("{=}");
+
+                // 将事件标记为已处理，以防止生成多个键盘事件
+         //       e.IsInputKey = true;
+         //   }
+        }
+        private void Ranks()
+        {
+            /*  得到光标行第一个字符的索引，
+             *  即从第1个字符开始到光标行的第1个字符索引*/
+            int index = richTextBox1.GetFirstCharIndexOfCurrentLine();
+            /*得到光标行的行号,第1行从0开始计算，习惯上我们是从1开始计算，所以+1。 */
+            int line = richTextBox1.GetLineFromCharIndex(index) + 1;
+            /*  SelectionStart得到光标所在位置的索引
+             *  再减去
+             *  当前行第一个字符的索引
+             *  = 光标所在的列数(从0开始)  */
+            int column = richTextBox1.SelectionStart - index + 1;
+
+
+
+
+            this.toolStripStatusLabel1.Text = string.Format("第：{0}行 {1}列,{2}", line.ToString(), column.ToString(), index.ToString());
+        }
+
+    }
+}
